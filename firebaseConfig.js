d782cfa511435626ba319d79a829e4c962880ffb
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-05-24 18:48:13 +0900

    [chore/#1]: 빌드파일 추가

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,19 @@
 // firebaseConfig.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDcs2E3lxGbqet9QLsI-Nj9AJNltlV5eW0",
-  authDomain: "mynihongo-5762d.firebaseapp.com",
-  projectId: "mynihongo-5762d",
-  storageBucket: "mynihongo-5762d.firebasestorage.app",
-  messagingSenderId: "439324955389",
-  appId: "1:439324955389:web:0be4a5dc03d0853682a494",
-  measurementId: "G-C778YZWJEW",
+  apiKey: 'AIzaSyDcs2E3lxGbqet9QLsI-Nj9AJNltlV5eW0',
+  authDomain: 'mynihongo-5762d.firebaseapp.com',
+  projectId: 'mynihongo-5762d',
+  storageBucket: 'mynihongo-5762d.firebasestorage.app',
+  messagingSenderId: '439324955389',
+  appId: '1:439324955389:web:0be4a5dc03d0853682a494',
+  measurementId: 'G-C778YZWJEW',
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
