df00521178ae8f309355ca12ab6a7fca49304ede
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-01 05:03:18 +0900

    [Feat/#7]: Quiz 화면 UI 구현

--- App.tsx
+++ App.tsx
@@ -3,13 +3,14 @@ import { StyleSheet } from 'react-native';
 import Main from './src/screens/Main';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Practice from './src/screens/Practice';
+import Quiz from './src/screens/Quiz';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.safeArea}>
         <StatusBar style="auto" />
-        <Practice />
+        <Quiz />
       </SafeAreaView>
     </SafeAreaProvider>
   );
