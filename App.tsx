7e94811cf77b98f3a3b57202555fdfa6c0578dc1
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-02 02:21:16 +0900

    [Feat/#11]: 테마 완료 화면 UI 구현

--- App.tsx
+++ App.tsx
@@ -5,13 +5,14 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Practice from './src/screens/Practice';
 import Quiz from './src/screens/Quiz';
 import Real from './src/screens/Real';
+import Complete from './src/screens/Complete';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.safeArea}>
         <StatusBar style="auto" />
-        <Real />
+        <Complete />
       </SafeAreaView>
     </SafeAreaProvider>
   );
