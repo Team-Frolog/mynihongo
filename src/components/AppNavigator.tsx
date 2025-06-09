d4156f5ac2641fbc61e3c2ce63b44e3aea6a1a6f
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-10 02:18:17 +0900

    [Feat/#15]: 로그인 판별 화면 전환 구현

---
+++ src/components/AppNavigator.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useUser } from '@/hooks/useUser';
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from 'firebaseConfig';
+import Login from '@/screens/Login';
+import Main from '@/screens/Main';
+import Practice from '@/screens/Practice';
+import Quiz from '@/screens/Quiz';
+import Real from '@/screens/Real';
+import Complete from '@/screens/Complete';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigator() {
+  const { createUser, isLoggedIn } = useUser();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        createUser(user.uid);
+      } else {
+        setIsLoading(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      setIsLoading(false);
+    }
+  }, [isLoggedIn]);
+
+  useEffect(() => {
+    if (!isLoading) {
+      SplashScreen.hideAsync();
+    }
+  }, [isLoading]);
+
+  if (isLoading) {
+    return null;
+  }
+
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      {isLoggedIn ? (
+        <>
+          <Stack.Screen name="Main" component={Main} />
+          <Stack.Screen name="Practice" component={Practice} />
+          <Stack.Screen name="Quiz" component={Quiz} />
+          <Stack.Screen name="Real" component={Real} />
+          <Stack.Screen name="Complete" component={Complete} />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={Login} />
+      )}
+    </Stack.Navigator>
+  );
+}
+
+export default AppNavigator;
