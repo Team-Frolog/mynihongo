25d09c9b0600dad69b9fbd1c50731d5852e58aa1
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-10 03:50:31 +0900

    [Feat/#15]: theme 학습 상태에 따른 학습 상태 컴포넌트 분기 처리

--- src/components/Main/ThemeList.tsx
+++ src/components/Main/ThemeList.tsx
@@ -3,26 +3,45 @@ import ChevronRight from '../../../assets/icons/ChevronRightArrow';
 import StudyProgressStatus from '@/components/Main/StudyProgressStatus';
 import { styles } from '@/styles/Main/ThemeList.style';
 import { themeData } from '@/data/theme';
+import { useUserStore } from '@/stores/useUserStore';
 
 function ThemeList() {
+  const themeStatus = useUserStore((state) => state.userInfo?.themeStatus);
+
   return (
     <FlatList
       contentContainerStyle={styles.list}
       data={themeData}
       showsVerticalScrollIndicator={false}
-      renderItem={({ item }) => (
-        <View style={styles.listItem}>
-          <View style={styles.titleWrapper}>
-            <Text style={styles.title}>{item.name}</Text>
-            <Text style={styles.titleHanza}>{item.japaneseName}</Text>
+      renderItem={({ item }) => {
+        const targetThemeData = themeStatus?.find(
+          (theme) => theme.themeName === item.id,
+        )!;
+
+        const status = targetThemeData?.status;
+        const words = targetThemeData?.words;
+
+        const listItemStyle = {
+          borderRightWidth: status ? 0 : 1,
+          paddingRight: status ? 0 : 15,
+        };
+
+        return (
+          <View style={[styles.listItem, listItemStyle]}>
+            <View style={styles.titleWrapper}>
+              <Text style={styles.title}>{item.name}</Text>
+              <Text style={styles.titleHanza}>{item.japaneseName}</Text>
+            </View>
+            <View style={styles.stepStatusWrapper}>
+              <Text style={styles.stepStatus}>{words ? words : 0} / 10</Text>
+              <ChevronRight width={24} height={24} />
+              {status && (
+                <StudyProgressStatus status={status} width={59} height={100} />
+              )}
+            </View>
           </View>
-          <View style={styles.stepStatusWrapper}>
-            <Text style={styles.stepStatus}>0 / 10</Text>
-            <ChevronRight width={24} height={24} />
-            <StudyProgressStatus width={59} height={100} />
-          </View>
-        </View>
-      )}
+        );
+      }}
       keyExtractor={(item) => item.id}
     />
   );
