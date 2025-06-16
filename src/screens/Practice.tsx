6eaec059440228cf775d29e2ea7304c8951efce3
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-17 01:38:50 +0900

    [Feat/#17]: 좌우 스와이프 효과 구현

--- src/screens/Practice.tsx
+++ src/screens/Practice.tsx
@@ -1,5 +1,4 @@
-import { View, Text, Image } from 'react-native';
-import Tts from '../../assets/icons/Tts';
+import { View, Text } from 'react-native';
 import GestureGuide from '../../assets/icons/GestureGuide';
 import Animated, {
   useSharedValue,
@@ -9,17 +8,38 @@ import Animated, {
   useAnimatedStyle,
   Easing,
 } from 'react-native-reanimated';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import BackHeader from '../components/commons/BackHeader';
 import { styles } from '@/styles/Practice/Practice.style';
 import { RouteProp, useRoute } from '@react-navigation/native';
 import { wordData } from '@/data/word';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import SwipeCard from '@/components/Practice/SwipeCard';
 
 function Practice() {
   const route = useRoute<RouteProp<any>>();
   const themeName = route.params?.themeName;
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isShowGestureGuide, setIsShowGestureGuide] = useState(true);
+
   const words = wordData.filter((word) => word.theme === themeName);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShowGestureGuide(false);
+    }, 3000);
+  }, []);
+
+  const handleSwipeLeft = () => {
+    console.log('모름 - 다시 학습');
+    setCurrentIndex((prev) => prev + 1);
+  };
+
+  const handleSwipeRight = () => {
+    console.log('앎 - 학습 완료');
+    setCurrentIndex((prev) => prev + 1);
+  };
 
   const translateX = useSharedValue(0);
 
@@ -40,29 +60,33 @@ function Practice() {
   }));
 
   return (
-    <View style={styles.container}>
-      <BackHeader title="공항" />
-      <View style={styles.wordContainer}>
-        <Image
-          source={require('../../assets/images/word-mock.png')}
-          style={styles.image}
-        />
-        <View style={styles.wordWrapper}>
-          <Text style={styles.jWord}>ひこう</Text>
-          <Text style={styles.kanji}>飛行</Text>
-          <Text style={styles.kWord}>비행</Text>
-          <Tts />
-        </View>
-        <View style={styles.gestureGuideWrapper}>
-          <Text style={styles.gestureGuide}>
-            모르면 왼쪽으로,{'\n'} 안다면 오른쪽으로 넘기세요
-          </Text>
-          <Animated.View style={animatedStyle}>
-            <GestureGuide />
-          </Animated.View>
-        </View>
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <BackHeader title="공항" />
+
+        {/* 다음 카드들을 미리 렌더링 (최대 3장) */}
+        {words.slice(currentIndex, currentIndex + 3).map((word, index) => (
+          <SwipeCard
+            key={word.id}
+            item={word}
+            onSwipeLeft={handleSwipeLeft}
+            onSwipeRight={handleSwipeRight}
+            index={index}
+            totalCards={3}
+          />
+        ))}
+        {isShowGestureGuide && (
+          <View style={styles.gestureGuideWrapper}>
+            <Text style={styles.gestureGuide}>
+              모르면 왼쪽으로,{'\n'} 안다면 오른쪽으로 넘기세요
+            </Text>
+            <Animated.View style={animatedStyle}>
+              <GestureGuide />
+            </Animated.View>
+          </View>
+        )}
       </View>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
