7fd63f6e082dda34873520728bae558d041b7c03
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-18 02:22:16 +0900

    [Feat/#17]: 학습 상태 즉시 업데이트

--- src/hooks/useWord.ts
+++ src/hooks/useWord.ts
@@ -1,14 +1,24 @@
 import { updateWordLearningStatus } from '@/services/word';
 import { useMutation } from '@tanstack/react-query';
 import { useUserStore } from '@/stores/useUserStore';
+import { useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
 export const useWord = () => {
+  const queryClient = useQueryClient();
   const userId = useUserStore((state) => state.userId);
+
+  useEffect(() => {
+    return () => {
+      queryClient.invalidateQueries({ queryKey: ['user', userId] });
+    };
+  }, [queryClient, userId]);
 
   const { mutate: updateWordToLearned } = useMutation({
     mutationFn: async (data: { wordId: string; themeId: string }) => {
       return await updateWordLearningStatus(userId, data.wordId, data.themeId);
     },
+
     onError: (error) => {
       console.log(error);
     },
