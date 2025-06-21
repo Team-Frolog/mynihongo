941c0841629a10ba0170b19a8c0862c32eafd3e3
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-22 02:39:29 +0900

    [Feat/#19]: 헤더 뒤로가기 버튼 기능 구현

--- src/components/commons/BackHeader.tsx
+++ src/components/commons/BackHeader.tsx
@@ -1,15 +1,24 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import ChevronLeftArrow from 'assets/icons/ChevronLeftArrow';
 import { styles } from '@/styles/commons/BackHeader.style';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 
 interface Props {
   title: string;
 }
 
 function BackHeader({ title }: Props) {
+  const navigation = useNavigation<NavigationProp<any>>();
+
+  const handleGoBack = () => {
+    navigation.navigate('Main');
+  };
+
   return (
     <View style={styles.header}>
-      <ChevronLeftArrow />
+      <TouchableOpacity style={styles.backButton} onPress={handleGoBack}>
+        <ChevronLeftArrow />
+      </TouchableOpacity>
       <Text style={styles.headerTitle}>{title}</Text>
     </View>
   );
