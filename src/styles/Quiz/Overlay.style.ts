9e2586858abeeb58f46b2b47c1fc3844bb5fba3c
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-22 19:43:06 +0900

    [Style/#19]: 오버레이 화면 스타일 추가

---
+++ src/styles/Quiz/Overlay.style.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 9999,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  },
+});
