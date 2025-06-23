44b76b4daa7bc6029ff530ac83086c11d8be1d29
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-23 19:38:59 +0900

    [Feat/#19]: 상태 업데이트 mutation 훅 구현

--- src/hooks/useWord.ts
+++ src/hooks/useWord.ts
@@ -1,12 +1,18 @@
-import { updateWordLearningStatus } from '@/services/word';
+import {
+  updateWordLearningStatus,
+  updateThemeLearnedStatus,
+} from '@/services/word';
 import { useMutation } from '@tanstack/react-query';
 import { useUserStore } from '@/stores/useUserStore';
 import { useQueryClient } from '@tanstack/react-query';
 import { useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 export const useWord = () => {
   const queryClient = useQueryClient();
   const userId = useUserStore((state) => state.userId);
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
 
   useEffect(() => {
     return () => {
@@ -18,11 +24,22 @@ export const useWord = () => {
     mutationFn: async (data: { wordId: string; themeId: string }) => {
       return await updateWordLearningStatus(userId, data.wordId, data.themeId);
     },
-
     onError: (error) => {
       console.log(error);
     },
   });
 
-  return { updateWordToLearned };
+  const { mutate: updateThemeStatus } = useMutation({
+    mutationFn: async (data: { themeId: string }) => {
+      return await updateThemeLearnedStatus(userId, data.themeId);
+    },
+    onSuccess: () => {
+      navigation.navigate('Conversation');
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  return { updateWordToLearned, updateThemeStatus };
 };
