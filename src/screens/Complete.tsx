9e59383590409b66e71dd95b0c10a36c508b7a04
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-25 02:49:16 +0900

    [Feat/#23]: 학습완료 버튼 기능 구현

--- src/screens/Complete.tsx
+++ src/screens/Complete.tsx
@@ -6,10 +6,13 @@ import { RouteProp, useRoute } from '@react-navigation/native';
 import { themeData } from '@/data/theme';
 import { wordData } from '@/data/word';
 import ListItem from '@/components/Complete/ListItem';
+import PressButton from '@/components/commons/PressButton';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
 
 function Complete() {
   const route = useRoute<RouteProp<any, 'Complete'>>();
   const themeId = route.params?.themeId;
+  const navigation = useNavigation<NavigationProp<any>>();
 
   const theme = useMemo(() => {
     return themeData.find((theme) => theme.id === themeId)!;
@@ -20,6 +23,14 @@ function Complete() {
   }, [themeId]);
 
   const themeName = theme.name;
+
+  const handlePressButton = (type: 'quiz' | 'conversation') => {
+    if (type === 'quiz') {
+      navigation.navigate('Quiz', { themeId });
+    } else {
+      navigation.navigate('Conversation', { themeId });
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -32,12 +43,18 @@ function Complete() {
         renderItem={({ item }) => <ListItem item={item} />}
       />
       <View style={styles.buttonWrapper}>
-        <Pressable style={styles.button}>
+        <PressButton
+          extraStyles={styles.button}
+          onPress={() => handlePressButton('quiz')}
+        >
           <Text style={styles.buttonText}>퀴즈</Text>
-        </Pressable>
-        <Pressable style={styles.button}>
+        </PressButton>
+        <PressButton
+          extraStyles={styles.button}
+          onPress={() => handlePressButton('conversation')}
+        >
           <Text style={styles.buttonText}>실전</Text>
-        </Pressable>
+        </PressButton>
       </View>
     </View>
   );
