50cacea4538cb02ad614c4fafd2a6e1923584384
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-25 02:43:33 +0900

    [Feat/#23]: 완료 단어 리스트 아이템 컴포넌트 구현

--- src/screens/Complete.tsx
+++ src/screens/Complete.tsx
@@ -1,71 +1,35 @@
 import BackHeader from '@/components/commons/BackHeader';
-import ChevronDownArrow from '../../assets/icons/ChevronDownArrow';
 import { FlatList, Pressable, Text, View } from 'react-native';
 import { styles } from '@/styles/Complete/Complete.style';
-import Tts from 'assets/icons/Tts';
-
-const list = [
-  {
-    id: 1,
-    word: '空港',
-    mean: '공항',
-    mean2: 'ひこう',
-  },
-  {
-    id: 2,
-    word: '空港',
-    mean: '공항',
-    mean2: 'ひこう',
-  },
-  {
-    id: 3,
-    word: '空港',
-    mean: '공항',
-    mean2: 'ひこう',
-  },
-  {
-    id: 4,
-    word: '空港',
-    mean: '공항',
-    mean2: 'ひこう',
-  },
-  {
-    id: 5,
-    word: '空港',
-    mean: '공항',
-    mean2: 'ひこう',
-  },
-  {
-    id: 6,
-    word: '空港',
-    mean: '공항',
-    mean2: 'ひこう',
-  },
-];
+import { useMemo } from 'react';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { themeData } from '@/data/theme';
+import { wordData } from '@/data/word';
+import ListItem from '@/components/Complete/ListItem';
 
 function Complete() {
+  const route = useRoute<RouteProp<any, 'Complete'>>();
+  const themeId = route.params?.themeId;
+
+  const theme = useMemo(() => {
+    return themeData.find((theme) => theme.id === themeId)!;
+  }, [themeId]);
+
+  const words = useMemo(() => {
+    return wordData.filter((word) => word.themeId === themeId);
+  }, [themeId]);
+
+  const themeName = theme.name;
+
   return (
     <View style={styles.container}>
-      <BackHeader title="공항" />
+      <BackHeader title={themeName} />
       <FlatList
         contentContainerStyle={styles.list}
-        data={list}
+        data={words}
         keyExtractor={(item) => item.id.toString()}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <View style={styles.contentWrapper}>
-            <View style={styles.wordWrapper}>
-              <ChevronDownArrow />
-              <Text style={styles.wordText}>{item.word}</Text>
-              <Tts />
-            </View>
-            <View style={styles.meanWrapper}>
-              <Text style={styles.meanText}>{item.mean}</Text>
-              <View style={styles.seperator}></View>
-              <Text style={styles.meanText}>{item.mean2}</Text>
-            </View>
-          </View>
-        )}
+        renderItem={({ item }) => <ListItem item={item} />}
       />
       <View style={styles.buttonWrapper}>
         <Pressable style={styles.button}>
