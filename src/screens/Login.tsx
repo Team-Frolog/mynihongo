3a0cc429e7c1fd06cbe83545318e1edf6e8a11cb
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-25 19:47:32 +0900

    [Style/#28]: 로그인 화면 ui 구현

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import {
-  GoogleSignin,
-  GoogleSigninButton,
-} from '@react-native-google-signin/google-signin';
+import { Text, View, ImageBackground } from 'react-native';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { signIn } from '@/services/auth';
 import { styles } from '@/styles/Login/Login.style';
+import PressButton from '@/components/commons/PressButton';
+import GoogleLoginIcon from '../../assets/icons/GoogleLoginIcon';
 
 GoogleSignin.configure({
   webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
@@ -14,16 +13,33 @@ GoogleSignin.configure({
 
 function Login() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <GoogleSigninButton
-        style={{ width: 192, height: 48 }}
-        size={GoogleSigninButton.Size.Wide}
-        color={GoogleSigninButton.Color.Dark}
-        onPress={signIn}
-      />
-    </View>
+    <ImageBackground
+      source={require('../../assets/images/login-background.png')}
+      style={styles.backgroundImage}
+      resizeMode="cover"
+    >
+      <StatusBar style="light" translucent />
+
+      <View style={styles.overlay}>
+        <View style={styles.container}>
+          <Text style={styles.title}>MYnihongo</Text>
+          <View style={styles.contentWrapper}>
+            <View style={styles.descriptionWrapper}>
+              <Text style={styles.description}>
+                배우고 바로 쓰는{'\n'}
+                일본어 공부
+              </Text>
+              <Text style={styles.subDescription}>
+                하루 10분으로 일본어 마스터!
+              </Text>
+            </View>
+            <PressButton onPress={signIn}>
+              <GoogleLoginIcon />
+            </PressButton>
+          </View>
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
 
--- src/styles/Login/Login.style.ts
+++ src/styles/Login/Login.style.ts
@@ -1,10 +1,49 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
 
 export const styles = StyleSheet.create({
+  backgroundImage: {
+    width: width,
+    height: height,
+    flex: 1,
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.4)', // 반투명 오버레이
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    paddingHorizontal: 24,
+    paddingTop: 50, // 상태바 영역 고려
+  },
+  title: {
+    transform: [{ translateY: -100 }],
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#fff',
+  },
+  contentWrapper: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 30,
+  },
+  descriptionWrapper: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 10,
+  },
+  description: {
+    fontSize: 32,
+    fontWeight: '900',
+    color: '#FAF8F4',
+    textAlign: 'center',
+  },
+  subDescription: {
+    fontSize: 16,
+    color: '#FAF8F4',
+    fontWeight: '500',
   },
 });
